b1b5486d4408cb068c6df7f971638ae421415681
Christian Fernandez <christian14@gmail.com>
2023-09-20 06:02:37 -0300

    Fix: incorporando que no se repita personajes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import  styleApp from './App.css';
 import Nav from './components/nav/Nav';
 import Cards from './components/cards/Cards';
-
-// Importando el hook useState.
 import { useState } from 'react';
 import axios from 'axios'
 
 function App() {
-   // Hooks
    const [characters, setCharacters] = useState([])
-
-
-   // Funciones y variables
-
-   // Revisar esta promesa, con la del instructor
+   
    const onSearch = id => {
       axios(`https://rickandmortyapi.com/api/character/${id}`)
          .then(({ data }) => {
-            // console.log(data)    
             if (data.name) {
-               //setCharacters([...characters, data]) 
-               setCharacters((oldChars) => [...oldChars, data]);
+               
+               // Comprobamos si el personaje ya existe en la lista
+               const characterExists = characters.some(character => character.id === data.id)
+               !characterExists 
+               ? // Si no existe lo agreagamos
+                  setCharacters((oldChars) => [...oldChars, data])
+               : window.alert('Este persona je ya se encuentra en pantalla')
+
             } else {
                window.alert('¡No hay personajes con este ID!');
             }
